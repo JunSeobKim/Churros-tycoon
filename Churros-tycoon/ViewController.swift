9a6b4584c5441845ab0240976bb05e585cb7ff52
JunSeobKim <wnstjq616@gmail.com>
2022-06-10 00:31:44 +0900

    Initial Commit

---
+++ Churros-tycoon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Churros-tycoon
+//
+//  Created by 김준섭 on 2022/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
